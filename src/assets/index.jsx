a64248fcb58ad9f362376720dd3b18da8b710a21
Boris Tenkes <boris.tenkes@gmail.com>
2023-02-05 12:53:38 +0100

    Initial commit

---
+++ src/assets/index.jsx
@@ -0,0 +1,40 @@
+import meeting from './about.jpg';
+import graduation from './background.jpg';
+import homeBanner from './banner.png';
+import classroom from './banner2.jpg';
+import basketball from './basketball.png';
+import cafeteria from './cafeteria.png';
+import certificate from './certificate.jpg';
+import library from './library.png';
+import logo from './logo.png';
+import london from './london.png';
+import newyork from './newyork.png';
+import washington from './washington.png';
+import avatar1 from './avatar1.jpg';
+import avatar2 from './avatar2.jpg';
+
+export const campus = {
+	london: london,
+	newyork: newyork,
+	washington: washington,
+};
+
+export const avatars = {
+	avatar1: avatar1,
+	avatar2: avatar2,
+};
+
+export const banners = {
+	home: homeBanner,
+	contact: graduation,
+	about: graduation,
+	courses: graduation,
+};
+
+export const facilities = {
+	library: library,
+	basketball: basketball,
+	cafeteria: cafeteria,
+};
+
+export { meeting, certificate, classroom, logo };
